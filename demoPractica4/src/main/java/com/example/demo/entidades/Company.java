1436c1958527b7630b6831c4d9f1d64e93b811ad
GabrielNF117 <47761386+GabrielNF117@users.noreply.github.com>
2019-03-19 18:15:35 -0700

    Add files via upload

---
+++ demoPractica4/src/main/java/com/example/demo/entidades/Company.java
@@ -0,0 +1,56 @@
+package com.example.demo.entidades;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "company")
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "version")
+    private Date version;
+
+    @ManyToOne
+    private CompanyStatus companyStatus;
+
+    public Company(String name, Date version) {
+        super();
+        this.name = name;
+        this.setVersion(version);
+    }
+
+    public Company(){
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getVersion() {
+        return version;
+    }
+
+    public void setVersion(Date version) {
+        this.version = version;
+    }
+}
---
+++ demoPractica4/src/main/java/com/example/demo/entidades/ContactDetail.java
@@ -0,0 +1,56 @@
+package com.example.demo.entidades;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "contactdetail")
+public class ContactDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "gender")
+    private int gender;
+
+    @Column(name = "dateOfBirth")
+    private Date dateOfBirth;
+
+    @ManyToOne
+    Contact contact;
+
+    public ContactDetail(int gender, Date dateOfBirth) {
+        super();
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public ContactDetail(){
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
